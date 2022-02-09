8043f860c2ebf235c419aaeb600c4190822147e2
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-02-09 16:21:31 -0500

    Update setupSUSE.sh

--- setupSUSE.sh
+++ setupSUSE.sh
@@ -2,7 +2,7 @@
 
 sudo zypper in qemu-tools qemu-kvm qemu-x86 qemu-audio-pa python3-pip  # for openSUSE Tumbleweed
 
-qemu-img create -f qcow2 macOS.qcow2 64G
+(ls macOS.qcow2 >> /dev/null 2>&1 && echo "") || qemu-img create -f qcow2 macOS.qcow2 64G
 
 ./fetch-macOS-v2.py
 
