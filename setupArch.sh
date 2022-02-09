fb00065329cba4d45f92e199e28c64de291fcd7f
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-02-09 16:21:04 -0500

    Update setupArch.sh

--- setupArch.sh
+++ setupArch.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -S qemu python python3-pip python-wheel
 
-qemu-img create -f qcow2 macOS.qcow2 64G
+(ls macOS.qcow2 >> /dev/null 2>&1 && echo "") || qemu-img create -f qcow2 macOS.qcow2 64G
 
 ./fetch-macOS-v2.py
 
