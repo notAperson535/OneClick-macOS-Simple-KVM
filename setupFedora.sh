ee732fcd8662e2dafd555cbf018f885f9171f714
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-02-09 16:21:12 -0500

    Update setupFedora.sh

--- setupFedora.sh
+++ setupFedora.sh
@@ -2,7 +2,7 @@
 
 sudo dnf install qemu qemu-img python3 python3-pip
 
-qemu-img create -f qcow2 macOS.qcow2 64G
+(ls macOS.qcow2 >> /dev/null 2>&1 && echo "") || qemu-img create -f qcow2 macOS.qcow2 64G
 
 ./fetch-macOS-v2.py
 
