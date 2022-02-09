33967268f95dda5145b3f5ed9fad5854d8170f34
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-02-09 15:33:59 -0500

    Create setupSUSE.sh

---
+++ setupSUSE.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo zypper in qemu-tools qemu-kvm qemu-x86 qemu-audio-pa python3-pip  # for openSUSE Tumbleweed
+
+qemu-img create -f qcow2 macOS.qcow2 64G
+
+./fetch-macOS-v2.py
+
+qemu-img convert BaseSystem.dmg -O raw BaseSystem.img
+
+sudo ./basic.sh
