069723942b51c47a4a24731d27d418d847161689
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-02-10 08:11:20 -0500

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -9,7 +9,7 @@ OVMF=$VMDIR/firmware
 
 qemu-system-x86_64 \
     -enable-kvm \
-    -m 2G \
+    -m 4G \
     -machine q35,accel=kvm \
     -smp 4,cores=2 \
     -cpu Penryn,vendor=GenuineIntel,kvm=on,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xsavec,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
