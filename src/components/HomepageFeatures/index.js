f9009934b3625edbc1c784e100cf97eba0470d48
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-05-19 15:56:48 -0400

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Run your needed apps',
-    Png: require('https://www.avanderlee.com/wp-content/uploads/2020/07/xcode-simulator-full-screen-mode.png').default,
+    Png: require('@site/static/img/safari.svg').default,
     description: (
       <>
         You can run XCode and Final Cut Pro
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: 'Or just use as a main OS',
-    Png: require('https://cdn.vox-cdn.com/thumbor/XmSqbwPFBDMwXfO9Jp_ImmrSO0M=/0x0:2562x1444/1120x0/filters:focal(0x0:2562x1444):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22867613/Screen_Shot_2021_09_21_at_9.13.23_AM.png').default,
+    Png: require('@site/static/img/xcode.svg').default,
     description: (
       <>
         If you allot the VM all of your memory and CPUs, you can use it as your main machine.
