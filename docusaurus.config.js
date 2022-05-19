493dd6c63aacc9b55cef2cdef43244ddec3994cb
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-05-19 19:45:57 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,7 +91,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} notAperson535.`,
       },
       prism: {
         theme: lightCodeTheme,
