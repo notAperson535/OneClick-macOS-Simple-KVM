1e233f421555b75eddb2ffea898060bcc47cc355
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-05-19 15:18:58 -0400

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Run your needed apps',
-    Jpg: require('https://is2-ssl.mzstatic.com/image/thumb/PurpleSource126/v4/5f/a6/51/5fa65193-8703-76aa-6a38-562d3ff849f1/b8946957-bf8e-4a55-937e-b516824a4294_1-_Documentation-dark.png/643x0w.jpg').default,
+    Jpg: require('@site/static/img/OneClick-macOS-Simple-KVM logo.svg',
     description: (
       <>
         You can run XCode and Final Cut Pro
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: 'Or just use as a main OS',
-    Png: require('https://cdn.vox-cdn.com/thumbor/kmPAYx3chBtbCO_u3j7UKP59YQQ=/0x0:2562x1444/1200x0/filters:focal(0x0:2562x1444):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22867613/Screen_Shot_2021_09_21_at_9.13.23_AM.png').default,
+    Png: require('@site/static/img/OneClick-macOS-Simple-KVM logo.svg',
     description: (
       <>
         If you allot the VM all of your memory and CPUs, you can use it as your main machine.
