8f2358b0cf7626ed1cbe0235e70ca80de638cceb
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-05-20 07:29:01 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/notAperson535/OneClick-macOS-Simple-KVM-docs/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/notAperson535/OneClick-macOS-Simple-KVM/tree/docs/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
