0586e60ecb32ee53483eb27a636bb5baa40b2218
Amber M <162277269+amber-m-mvd@users.noreply.github.com>
2024-06-13 09:33:21 +1000

    Implement sourcery-ai's suggestions
    They did make a good point...

--- windows-install.sh
+++ windows-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt update && sudo apt -y upgrade
-sudo apt -y install build-essential libncurses-dev bison flex libssl-dev libelf-dev cpu-checker qemu-kvm aria2
-sudo apt-get install qemu-system qemu-utils python3 python3-pip -y
+sudo apt-get -y install qemu-system qemu-utils python3 python3-pip build-essential libncurses-dev bison flex libssl-dev libelf-dev cpu-checker qemu-kvm aria2
 cd ~
 aria2c -x 10 https://github.com/microsoft/WSL2-Linux-Kernel/archive/linux-msft-wsl-5.15.153.1.tar.gz
 tar -xf linux-msft-wsl-5.15.153.1.tar.gz
