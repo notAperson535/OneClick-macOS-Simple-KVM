63faf92d0d5ce162b94493b64effc61719d943f3
Amber M <162277269+amber-m-mvd@users.noreply.github.com>
2024-06-13 10:23:48 +1000

    Add missing make dependency

--- windows-install.sh
+++ windows-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update && sudo apt -y upgrade
-sudo apt-get -y install qemu-system qemu-utils python3 python3-pip build-essential libncurses-dev bison flex libssl-dev libelf-dev cpu-checker qemu-kvm aria2
+sudo apt-get -y install qemu-system qemu-utils python3 python3-pip build-essential libncurses-dev bison flex libssl-dev libelf-dev cpu-checker qemu-kvm aria2 bc
 cd ~
 aria2c -x 10 https://github.com/microsoft/WSL2-Linux-Kernel/archive/linux-msft-wsl-5.15.153.1.tar.gz
 tar -xf WSL2-Linux-Kernel-linux-msft-wsl-5.15.153.1.tar.gz
